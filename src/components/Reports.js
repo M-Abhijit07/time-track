84ad0d8e679af8b6b55635b3ed2806fc2b825d25
Abhijit Mondal <b210039@nitsikkim.ac.in>
2023-10-27 21:11:45 +0530

    added time display and profile data

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { startOfWeek, endOfDay, startOfMonth, endOfMonth, isWithinInterval, addMilliseconds, format } from "date-fns";
 import { AiOutlineLogout } from "react-icons/ai";
 import { getAuth } from "firebase/auth";
 import { collection, getFirestore, query, where, onSnapshot, QuerySnapshot } from "firebase/firestore";
@@ -35,17 +36,56 @@ function Reports() {
           const q = query(collection(db, "tasks"), where('userId', "==", auth.currentUser.uid) );
           //on Snapshot
           const unsubscribe = onSnapshot(q, (querySnapshot)=>{
-            //get data from db
-            setTasks(
-              querySnapshot.docs.map((doc)=>{
-                return {
-                  ...doc.data(),
-                  id: doc.id,
-                  date: new Date(doc.data().startTime).toISOString(),
+            if(querySnapshot.docs.length){
+              let weekTotal = 0;
+              let monthTotal = 0;
+              let total = 0;
+              const now = new Date();
+
+              const weekStart = startOfWeek(now);
+              const weekEnd = endOfDay(now);
+              const monthStart = startOfMonth(now);
+              const monthEnd = endOfMonth(now);
+
+              //Get date from task
+              querySnapshot.docs.forEach((doc) => {
+                const data = doc.data();
+                const taskDate = new Date(data.startTime);
+                const taskTime = data.totalTime || 0;
+                if(isWithinInterval(taskDate, {start: weekStart, end:weekEnd})) {
+                  weekTotal += taskTime;
                 }
-              })
-            )
+                if(isWithinInterval(taskDate, {start: monthStart, end:monthEnd})) {
+                  monthTotal += taskTime;
+                }
+                total += taskTime;
+              });
+              console.log({
+                thisWeekTotal,
+                thisMonthTotal,
+                totalTime,
+                weekTotal,
+                monthTotal,
+                total
+              });
+              //Update State
+              setThisWeekTotal(weekTotal);
+              setThisMonthTotal(monthTotal);
+              setTotalTime(total);
+              //get data from db
+              setTasks(
+                querySnapshot.docs.map((doc)=>{
+                  return {
+                    ...doc.data(),
+                    id: doc.id,
+                    date: new Date(doc.data().startTime).toISOString(),
+                  }
+                })
+              )
+            }
           });
+          //CLean up
+          return unsubscribe;
         }else{
           setError("Please SignIn");
           setLoading(false);
@@ -65,6 +105,15 @@ function Reports() {
       }
     };
   },[]);
+
+  //------------
+  //Format Time
+  //------------
+  const formatTime = (timeInMillis)=>{
+    const date = addMilliseconds(new Date(0), timeInMillis);
+    return format(date, "HH:mm:ss");
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
       <div className="container mx-auto px-4 py-10">
@@ -77,11 +126,11 @@ function Reports() {
         <div className="bg-white p-4 my-6 rounded-md text-black max-w-md mx-auto">
           <h2 className="text-lg font-semibold mb-2">User Profile</h2>
           <div className="flex items-center">
-            <img className="h-16 w-16 rounded-full" src="" alt="profile" />
+            <img className="h-16 w-16 rounded-full" src={auth.currentUser.photoURL} alt="profile" />
             <div className="ml-4">
-              <h3 className="text-lg font-semibold">Username</h3>
-              <p className="text-gray-600">Email</p>
-              <p className="text-gray-600">Last Login</p>
+              <h3 className="text-lg font-semibold">{auth.currentUser.displayName || "user name"}</h3>
+              <p className="text-gray-600">{auth.currentUser.email}</p>
+              <p className="text-gray-600">Last Login : {auth.currentUser.metadata.lastSignInTime}</p>
             </div>
           </div>
         </div>
@@ -89,15 +138,15 @@ function Reports() {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-center mb-8">
           <div className="bg-gradient-to-r from-green-400 to-blue-500 p-4 rounded-md text-white shadow-lg">
             <h2 className="text-lg font-semibold">This Week</h2>
-            <p className="text-2xl font-bold">20</p>
+            <p className="text-2xl font-bold">{formatTime(thisWeekTotal)}</p>
           </div>
           <div className="bg-gradient-to-r from-purple-400 to-pink-500 p-4 rounded-md text-white shadow-lg">
             <h2 className="text-lg font-semibold">This Month</h2>
-            <p className="text-2xl font-bold">10</p>
+            <p className="text-2xl font-bold">{formatTime(thisMonthTotal)}</p>
           </div>
           <div className="bg-gradient-to-r from-red-400 to-yellow-500 p-4 rounded-md text-white shadow-lg">
             <h2 className="text-lg font-semibold">Total</h2>
-            <p className="text-2xl font-bold">30</p>
+            <p className="text-2xl font-bold">{formatTime(totalTime)}</p>
           </div>
         </div>
 
