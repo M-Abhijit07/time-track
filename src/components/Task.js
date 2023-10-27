993f46e621dffd34658cc8909987a58cbeeeee77
Abhijit Mondal <b210039@nitsikkim.ac.in>
2023-10-27 19:13:06 +0530

    status functionality added to buttons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-
+import { getFirestore,updateDoc, onSnapshot, doc } from "firebase/firestore";
 import { BsCircleFill } from "react-icons/bs";
+import { format } from 'date-fns';
 import {
   AiOutlineEdit,
   AiOutlineDelete,
@@ -10,6 +11,10 @@ import {
   AiOutlinePauseCircle, 
   AiOutlineReload
 } from "react-icons/ai";
+import app from "../firebase/config";
+
+//Instatnce of FireStore
+const db = getFirestore(app);
 
 function Task({task}) {
   //-------------
@@ -37,10 +42,51 @@ function Task({task}) {
   const handleRenderTaskDescription = () => {};
 
   //Handle start
-  const handleStart = () => {};
+  const handleStart = async () => {
+    try {
+      await updateDoc(doc(db, 'tasks', localTask.id),{
+        status: 'in_progress',
+        startTime: Date.now(),
+      });
+      const taskDoc = doc(db, 'tasks', localTask.id);
+      onSnapshot(taskDoc, (docSnap)=>{
+        if(docSnap.exists()){
+          setLocalTask({
+            ...docSnap.data(),
+            date: localTask.date,
+            id: localTask.id
+          })
+        }
+      })
+    } catch (error) {
+      console.log('Error starting task: ', error);
+    }
+  };
 
   //Handle pause
-  const handlePause = () => {};
+  const handlePause = async () => {
+    try {
+      const elapsed = localTask.startTime ? Date.now() - localTask.startTime : 0;
+      const newTotalTime = (localTask.totalTime || 0) + elapsed;
+      await updateDoc(doc(db, 'tasks', localTask.id), {
+        status : 'paused',
+        endTime : Date.now(),
+        totalTime : newTotalTime,
+      });
+      const taskDoc = doc(db, 'tasks', localTask.id);
+      onSnapshot(taskDoc, (docSnap)=>{
+        if(docSnap.exists()) {
+          setLocalTask({
+          ...docSnap.data(),
+            date: localTask.date,
+            id: localTask.id
+          });
+        }
+      });
+    } catch (error) {
+      console.log('Error in pausing task: ',error);
+    }
+  };
 
   //Handle delete
   const handleDelete = () => {};
@@ -69,15 +115,20 @@ function Task({task}) {
   return (
     <div className="bg-white p-4 rounded-md text-black shadow-lg flex flex-col md:flex-row md:items-center justify-between">
       <div className="md:space-x-2 space-y-2 md:space-y-0">
-        {/* render buttons */}
+        {/* render description */}
         <div className="flex items-center space-x-2">
           <AiOutlineCalendar className="text-gray-600" />
+          <p className="text-gray-600">{
+            format(new Date(localTask.date), 'dd/MM/yyyy')
+          }</p>
           <p className="text-gray-600">{task.task}</p>
         </div>
       </div>
       <div className="flex items-center space-x-2 justify-center">
-        <BsCircleFill />
-        <p>status</p>
+        <BsCircleFill color={
+          localTask.status === 'paused' ? 'red' : localTask.status === 'in_progress' ? 'green' : 'yellow'
+        }/>
+        <p>{localTask.status}</p>
       </div>
       <div className="flex items-center space-x-2 justify-center md:justify-end">
         {/* Render buttons */}
