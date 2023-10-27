7d42411b4d5f4183e8cf3cdbc25f8db9e8acb78c
Abhijit Mondal <b210039@nitsikkim.ac.in>
2023-10-27 18:38:00 +0530

    added action button in tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,12 +5,19 @@ import {
   AiOutlineEdit,
   AiOutlineDelete,
   AiOutlineCalendar,
+  AiOutlineCiCircle,
+  AiOutlinePlayCircle,
+  AiOutlinePauseCircle, 
+  AiOutlineReload
 } from "react-icons/ai";
 
 function Task({task}) {
   //-------------
   //Local States
   //-------------
+  const [localTask, setLocalTask] = useState(task);
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTaskDescription, setNewTaskDescription] = useState(localTask.task);
 
   //Handle Edit
   const handleEdit = () => {
@@ -19,7 +26,7 @@ function Task({task}) {
   //Handle Edit
   const handleCancelEdit = () => {
     setIsEditing = false;
-    setNewTaskDescription(localState.task);
+    setNewTaskDescription(localTask.task);
   };
 
   //Handle Edit
@@ -39,11 +46,26 @@ function Task({task}) {
   const handleDelete = () => {};
 
   //Handle render buttons
-  const handleRenderButtons = () => {};
+  const handleRenderButtons = () => {
+    switch(localTask.status){
+      case 'unstarted':
+        return (
+          <AiOutlinePlayCircle className='text-2xl text-purple-400' onClick={handleStart} />
+        )
 
-  const [localState, setLocalState] = useState(task);
-  const [isEditing, setIsEditing] = useState(false);
-  const [newTaskDescription, setNewTaskDescription] = useState(localState.task);
+      case 'in_progress':
+        return (
+          <AiOutlinePauseCircle className='text-2xl text-green-400' onClick={handlePause} />
+        )
+
+      default:
+        case "unstarted":
+        return (
+          <AiOutlineReload className='text-2xl text-green-400' onClick={handleStart} />
+        )
+    }
+  };
+
   return (
     <div className="bg-white p-4 rounded-md text-black shadow-lg flex flex-col md:flex-row md:items-center justify-between">
       <div className="md:space-x-2 space-y-2 md:space-y-0">
@@ -59,6 +81,7 @@ function Task({task}) {
       </div>
       <div className="flex items-center space-x-2 justify-center md:justify-end">
         {/* Render buttons */}
+        {handleRenderButtons()}
         <AiOutlineEdit className="text-2xl text-purple-400" />
         <AiOutlineDelete className="text-2xl text-red-500" />
       </div>
